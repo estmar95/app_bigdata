ac64288c097f9625acf0e2647f340706249c1335
Yachay J. Tolosa <yachay@gmail.com>
2024-05-27 10:59:56 -0500

    Se ajusta el barco y la fecha por defecto

--- app/app.py
+++ app/app.py
@@ -24,6 +24,7 @@ def get_data_calor(mes):
     items = [[*i["geometry"], np.log(i["count"])] for i in items]
     return items
 
+
 def get_data(barco, dia):
     db = client.geo
     items = db.rutas.find({"_id": f"{barco}-{dia}"})
@@ -38,10 +39,13 @@ def get_barcos():
     items = [i["MMSI"] for i in items]
     return items
 
+
+### -------------------------------------------- MAPA DE CALOR  -----------------------------------------------
+
+
 st.title('Tráfico de barcos por mes')
 
-mes = st.selectbox('Mes', options=[ f"{i}" if i > 9 else f"0{i}" for i in range(1,13)])
-
+mes = st.selectbox('Mes', options=[f"{i}" if i > 9 else f"0{i}" for i in range(1, 13)])
 
 heatmap_data = get_data_calor(mes)
 
@@ -49,17 +53,14 @@ m = folium.Map(location=(29.949932, -90.070116), zoom_start=7)
 HeatMap(heatmap_data, radius=7, blur=2, ).add_to(m)
 folium_static(m)
 
-
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-
+### -------------------------------------------- RUTAS DE BARCOS -----------------------------------------------
 
 st.title('Rutas de barcos')
 
 barcos = get_barcos()
-barco = st.selectbox('Seleccione el baro', options=barcos)
+barco = st.selectbox('Seleccione el baro', options=barcos, index=barcos.index(235009420))
 
-dia = st.date_input('Seleccione el ía', min_value=date(2020, 1, 1), max_value=date(2020, 12, 31))
+dia = st.date_input('Seleccione el ía', min_value=date(2020, 1, 1), max_value=date(2020, 12, 31), value=date(2020, 6, 3))
 
 items = get_data(barco, dia)
 
